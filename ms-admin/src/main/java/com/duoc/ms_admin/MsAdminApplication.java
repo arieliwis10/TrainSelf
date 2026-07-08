59f1e0274b454114342ae456f7153083e9c7b64b
Ariel Galvez <ari.galvez@duocuc.cl>
2026-07-07 22:43:39 -0400

    agregar BFF y microservicios (ms-usuarios, ms-rutinas, ms-leaderboard, ms-admin)
    - Renombrado backend a bff (Spring Boot 3.5.16)
    - Creado ms-usuarios en puerto 8082
    - Creado ms-rutinas en puerto 8081
    - Creado ms-leaderboard en puerto 8083
    - Creado ms-admin en puerto 8084
    - Arquitectura de microservicios lista para implementar endpoints

---
+++ ms-admin/src/main/java/com/duoc/ms_admin/MsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.ms_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdminApplication.class, args);
+	}
+
+}
---
+++ ms-leaderboard/src/main/java/com/duoc/ms_leaderboard/MsLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.ms_leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLeaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLeaderboardApplication.class, args);
+	}
+
+}
