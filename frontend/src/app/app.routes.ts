0ad8176fdad0c62c7c88e2a9c38f267d781bd2d2
Ariel Galvez <ari.galvez@duocuc.cl>
2026-07-13 21:27:57 -0400

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -17,8 +17,8 @@ export const routes: Routes = [
   { path: 'objetivo', component: ObjetivoComponent, canActivate: [authGuard] },
   { path: 'rutina/:id', component: RutinaComponent, canActivate: [authGuard] },
   { path: 'leaderboard', component: LeaderboardComponent, canActivate: [authGuard] },
-  { path: 'admin', component: AdminComponent, canActivate: [authGuard, adminGuard] },
-  { path: '**', redirectTo: 'login' },
   { path: 'perfil/:id', component: PerfilComponent, canActivate: [authGuard] },
-  { path: 'admin/rutinas', component: AdminRutinasComponent, canActivate: [authGuard, adminGuard] }
+  { path: 'admin', component: AdminComponent, canActivate: [authGuard, adminGuard] },
+  { path: 'admin/rutinas', component: AdminRutinasComponent, canActivate: [authGuard, adminGuard] },
+  { path: '**', redirectTo: 'login' }
 ];
